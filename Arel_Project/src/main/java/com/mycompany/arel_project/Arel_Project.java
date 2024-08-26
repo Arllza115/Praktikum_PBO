47aa2c6caf47193eee82aa2020783669d46c8eeb
Arllza115 <projecthahahihi58@gmail.com>
2024-08-26 15:12:23 +0700

    Praktikum PBO
    PBO

---
+++ Arel_Project/src/main/java/com/mycompany/arel_project/Arel_Project.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.arel_project;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Arel_Project {
+
+    public static void main(String[] args) {
+        String nama, alamat, npm, no_hp;
+        
+       
+        
+        nama = "Arel Lioza Akhmad";
+        alamat = "Amsterdam, USA";
+        npm = "2320506050";
+        no_hp = "089518950106";
+        
+        
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM : " + npm);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("NO HP : " + no_hp);
+ 
+        
+        
+       
+       
+        
+        
+    }
+}
